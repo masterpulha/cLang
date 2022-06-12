965019de88544ab885ab9449dddd6bd3b846f02b
Pedro <ptos666@gmail.com>
2022-06-12 20:33:08 +0100

    Add files via upload

--- calculator/calculator.c
+++ calculator/calculator.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 
-float input1[30], input2[30];
-char user_choice[30];
-int i = 0, d = 0;
+int loop_options = 0, loop_run = 0;
+float input1, input2;
+char user_choice[9], s[3];
+char *ary_quit[1][17] = {"y", "Y", "yes", "Yes", "YES", "Sim", "sim", "SIM", "si", "SI", "Si", "ja", "JA", "Ja", "Oui", "oui", "OUI"};
+char *ary_choices[1][20] = {"1", "Addition", "Add", "addition", "add", "2", "Subtraction", "Subtract", "subtraction", "subtract", "3", "Multiplication", "Multiply", "multiplication", "multiply", "4", "Division", "Divide", "division", "divide"};
 
+int strcmp();
 int ft_restart(void);
 void ft_print_logo(void);
 char ft_user_choice(void);
@@ -16,9 +19,9 @@ void ft_return_result(float a, float b, float c, int d);
 int main(void)
 {
     ft_print_logo();
-    while (d == 0)
+    while (loop_run == 0)
     {
-        while (i == 0)
+        while (loop_options == 0)
         {
             ft_user_choice();
             ft_validate_choice(user_choice);
@@ -35,69 +38,48 @@ int main(void)
 
 // Functions:
 
-int ft_restart()
+void ft_print_logo(void)
 {
-    printf("\nWould you like to quit? [Y/N]\n");
-    char a[3];
-    scanf("%s", a);
-    if ((strcmp(a, "y") == 0) ||
-        (strcmp(a, "Y") == 0) ||
-        (strcmp(a, "yes") == 0) ||
-        (strcmp(a, "Yes") == 0) ||
-        (strcmp(a, "YES") == 0) ||
-        (strcmp(a, "Sim") == 0) ||
-        (strcmp(a, "sim") == 0) ||
-        (strcmp(a, "SIM") == 0) ||
-        (strcmp(a, "si") == 0) ||
-        (strcmp(a, "ja") == 0) ||
-        (strcmp(a, "JA") == 0) ||
-        (strcmp(a, "Ja") == 0) ||
-        (strcmp(a, "Si") == 0) ||
-        (strcmp(a, "SI") == 0) ||
-        (strcmp(a, "Oui") == 0) ||
-        (strcmp(a, "oui") == 0) ||
-        (strcmp(a, "OUI") == 0))
-    {
-        d = 1;
-        return d;
-    }
-    else
-    {
-        i = 0;
-        return i;
-    }
+    printf("\nWelcome To:\n");
+    // Font: Cyberlarge
+    printf(" _______ _______ _______ _     _\n");
+    printf(" |  |  | |_____|    |    |_____|\n");
+    printf(" |  |  | |     |    |    |     |\n");
+}
+
+char ft_user_choice(void)
+{
+    printf("\nChose an operation:\n\n");
+    printf("1: Addition\n2: Subtraction\n3: Multiplication\n4: Division\n");
+    printf("\nType Your Choice: ");
+    scanf("%s", user_choice);
+    return user_choice;
 }
 
 int ft_validate_choice(char *a)
 {
-    if ((strcmp(a, "1") == 0) ||
-        (strcmp(a, "Addition") == 0) ||
-        (strcmp(a, "Add") == 0) ||
-        (strcmp(a, "addition") == 0) ||
-        (strcmp(a, "add") == 0) ||
-        (strcmp(a, "2") == 0) ||
-        (strcmp(a, "Subtraction") == 0) ||
-        (strcmp(a, "Subtract") == 0) ||
-        (strcmp(a, "subtraction") == 0) ||
-        (strcmp(a, "subtract") == 0) ||
-        (strcmp(a, "3") == 0) ||
-        (strcmp(a, "Multiplication") == 0) ||
-        (strcmp(a, "Multiply") == 0) ||
-        (strcmp(a, "multiplication") == 0) ||
-        (strcmp(a, "multiply") == 0) ||
-        (strcmp(a, "4") == 0) ||
-        (strcmp(a, "Division") == 0) ||
-        (strcmp(a, "Divide") == 0) ||
-        (strcmp(a, "division") == 0) ||
-        (strcmp(a, "divide") == 0))
+    int loop = 0;
+    while (loop < (sizeof(ary_choices) / 8))
     {
-        i = 1;
-        return i;
+        if (strcmp(a, ary_choices[0][loop]) == 0)
+        {
+
+            loop_options = 1;
+            break;
+        }
+        else
+        {
+            loop_options = 0;
+        }
+        loop++;
     }
-    else
+    if (loop_options == 0)
     {
-        printf("\n%s is not a valid option, please try again!\n\n", a);
+        printf("\n############# W A R N I N G ! ##################\n");
+        printf("\n%s is not a valid option, please try again!\n", a);
     }
+
+    return loop_options;
 }
 
 int ft_convert_user_choice(char *a)
@@ -140,47 +122,18 @@ int ft_convert_user_choice(char *a)
     }
 }
 
-void ft_return_result(float a, float b, float c, int d)
-{
-    if (d == 1)
-    {
-        printf("%g + %g = %g\n", a, b, c);
-    }
-    if (d == 2)
-    {
-        printf("%g - %g = %g\n", a, b, c);
-    }
-    if (d == 3)
-    {
-        printf("%g * %g = %g\n", a, b, c);
-    }
-    if (d == 4)
-    {
-        printf("%g / %g = %g\n", a, b, c);
-    }
-}
-
-char ft_user_choice(void)
-{
-    printf("Chose an operation:\n\n");
-    printf("1: Addition\n2: Subtraction\n3: Multiplication\n4: Division\n");
-    printf("\nType Your Choice:");
-    scanf("%s", user_choice);
-    return user_choice;
-}
-
 float ft_ask_numbers(int a)
 {
     if (a == 1)
     {
-        printf("Enter a number\n");
+        printf("\nEnter a number\n");
         float inputed;
         scanf("%g", &inputed);
         return inputed;
     }
     if (a == 2)
     {
-        printf("Enter another number\n");
+        printf("\nEnter another number\n");
         float inputed;
         scanf("%g", &inputed);
         return inputed;
@@ -211,11 +164,46 @@ float ft_do_math(float a, float b, int c)
     }
 }
 
-void ft_print_logo(void)
+void ft_return_result(float a, float b, float c, int d)
 {
-    printf("\nWelcome To:\n");
-    // Font: Cyberlarge
-    printf(" _______ _______ _______ _     _\n");
-    printf(" |  |  | |_____|    |    |_____|\n");
-    printf(" |  |  | |     |    |    |     |\n\n");
+    if (d == 1)
+    {
+        printf("\n%g + %g = %g\n", a, b, c);
+    }
+    if (d == 2)
+    {
+        printf("\n%g - %g = %g\n", a, b, c);
+    }
+    if (d == 3)
+    {
+        printf("\n%g * %g = %g\n", a, b, c);
+    }
+    if (d == 4)
+    {
+        printf("\n%g / %g = %g\n", a, b, c);
+    }
+}
+
+int ft_restart()
+{
+    printf("\nWould you like to quit?\n\n");
+    int loop = 0;
+    scanf("%s", s);
+    while (loop < (sizeof(ary_quit) / 8))
+    {
+        if (strcmp(s, ary_quit[0][loop]) == 0)
+        {
+            printf("\n#############     B Y E !     ##################\n\n");
+            loop_run = 1;
+            return loop_run;
+            break;
+        }
+        else
+        {
+            loop_options = 0;
+        }
+        loop++;
+    }
+ 
+    return loop_run;
 }
