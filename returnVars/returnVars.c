d0976348e419d48e293fc5f7091e37f015d2ca80
Pedro <ptos666@gmail.com>
2022-06-29 13:36:26 +0100

    Update returnVars.c

--- returnVars/returnVars.c
+++ returnVars/returnVars.c
@@ -4,20 +4,20 @@
 int main(void)
 {
     SetConsoleTitle("ReturnVars");
-    int numberInside = 1337;
+    int number = 1337;
     int count = 0;
     char name[] = "Pedro";
     char quit[] = "no";
 
     while ((strcmp(quit, "no") == 0))
     {
-        printf("\nNumber is: %i\nmemAddr: 0x%0.8p\n", numberInside, &numberInside);
+        printf("\nNumber is: %i\nmemAddr: 0x%0.8p\n", number, &number);
         printf("\nCount is: %i\nmemAddr: 0x%0.8p\n", count, &count);
         printf("\nName: %s\nmemAddr: 0x%0.8p\n", name, &name);
         printf("\nDo you want to quit?\n");
         scanf("%s", quit);
         count++;
     }
-    
+
     return 0;
 }
